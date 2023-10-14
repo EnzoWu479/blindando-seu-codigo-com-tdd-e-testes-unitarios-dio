d0927940dde1ceed4e801a189107254d78cb3111
EnzoWu479 <enwu2014@hotmail.com>
2023-10-14 13:57:15 -0300

    feat: done

--- NewTalents/Services/Calculadora.cs
+++ NewTalents/Services/Calculadora.cs
@@ -7,20 +7,33 @@ namespace NewTalents.Services
 {
     public class Calculadora
     {
+        private List<string> _historico = new List<string>();
         public int Somar(int a, int b) {
-            return 0;
+            var resultado = a + b;
+            _historico.Insert(0, "Res: " + resultado);
+            return resultado;
         }
         public int Subtrair(int a, int b) {
-            return 0;
+            var resultado = a - b;
+             _historico.Insert(0, "Res: " + resultado);
+            return resultado;
         }
         public int Multiplicar(int a, int b) {
-            return 0;
+            var resultado = a * b;
+            _historico.Insert(0, "Res: " + resultado);
+            return resultado;
         } 
         public int Dividir(int a, int b) {
-            return 0;
+            if (b == 0) throw new DivideByZeroException();
+
+            var resultado = a / b;
+            _historico.Insert(0, "Res: " + resultado);
+            
+            return resultado;
         }
         public List<string> Historico() {
-            return new List<string>();
+            _historico.RemoveRange(3, _historico.Count - 3);
+            return _historico;
         }
     }
 }
