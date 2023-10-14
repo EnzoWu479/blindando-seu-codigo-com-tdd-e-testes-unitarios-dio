af7c9645432dcf8e041d240db0d9ff13154cc8c9
EnzoWu479 <enwu2014@hotmail.com>
2023-10-14 13:42:57 -0300

    Project settings

---
+++ NewTalents/Services/Calculadora.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewTalents.Services
+{
+    public class Calculadora
+    {
+        public int Somar(int a, int b) {
+            return 0;
+        }
+        public int Subtrair(int a, int b) {
+            return 0;
+        }
+        public int Multiplicar(int a, int b) {
+            return 0;
+        } 
+        public int Dividir(int a, int b) {
+            return 0;
+        }
+        public List<string> Historico() {
+            return new List<string>();
+        }
+    }
+}
